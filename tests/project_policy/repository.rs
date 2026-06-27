61b537941f8fca1c6488f9a41ae86c762d980bdf
vicotrbb <victor.bona@hotmail.com>
2026-06-27 18:17:04 -0300

    fix(proof): reduce screenshot lookup race

--- tests/project_policy/repository.rs
+++ tests/project_policy/repository.rs
@@ -198,6 +198,7 @@ fn distribution_assets_keep_desktop_identity() {
     assert!(screenshot_script.contains("screencapture -x -l"));
     assert!(screenshot_script.contains("screencapture -x -R"));
     assert!(screenshot_script.contains("NSBitmapImageRep"));
+    assert!(screenshot_script.contains("GROMAQ_SCREENSHOT_DELAY_SECONDS:-0.05"));
     assert!(screenshot_script.contains("GROMAQ_SCREENSHOT_MIN_BACKGROUND_PIXELS"));
     assert!(screenshot_script.contains("rm -f \"${output}\""));
     assert!(desktop.contains("Icon=dev.gromaq.Gromaq"));
